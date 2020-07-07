00b19059725065d79e1094a8b2e6cf79e4a648b0
jujix <18301246+jujix@users.noreply.github.com>
2020-07-07 16:45:37 +0300

    Add login and logout routes

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from "express";
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get("/login", (req: Request, res: Response) => {
@@ -18,10 +22,37 @@ router.get("/login", (req: Request, res: Response) => {
   `);
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  res.send(email + password);
+  if (email && password && email === "hi@hi.com" && password === "password") {
+    req.session = { loggedIn: true };
+    res.redirect("/");
+  } else {
+  }
+});
+
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+      <div>You are logged in</div>
+      <a href="/logout">Logout</a>
+      </div>
+      `);
+  } else {
+    res.send(`
+    <div>
+    <div>You are not logged in</div>
+    <a href="/login">Login</a>
+    </div>
+    `);
+  }
+});
+
+router.get("/logout", (req: Request, res: Response) => {
+  req.session = null;
+  res.redirect("/");
 });
 
 export { router };
